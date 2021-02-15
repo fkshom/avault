6ee8977f3398f80a9fe5b0200b06abfe82321a63
Shoma FUKUDA <fkshom@gmail.com>
2021-02-16 01:51:10 +0900

    wip

---
+++ avault.py
@@ -0,0 +1,129 @@
+#!/usr/bin/env python3
+
+import argparse
+import subprocess
+import tempfile
+import os
+import yaml
+from pprint import pprint as pp
+
+class AnsibleVault():
+    def __init__(self, filename, password_sets):
+        self.filename = filename
+        self.password_sets = password_sets
+
+    def is_whole_vaulted(self):
+        with open(self.filename, 'r') as f:
+            if f.readline().strip().startswith("$ANSIBLE_VAULT"):
+                return True
+        return False
+
+    def _run_process(self, command):
+        for password_set in self.password_sets:
+            try:
+                with tempfile.NamedTemporaryFile("w+") as f:
+                    print(password_set['password'], file=f)
+                    f.seek(0)
+                    subprocess.run(f'ansible-vault {command} --vault-password-file {f.name} {self.filename}', shell=True, check=True)
+                    break
+            except subprocess.CalledProcessError as e:
+                pass
+            except Exception as e:
+                raise e
+        else:
+            raise Exception("Decrypt Error")
+
+    def decrypt(self):
+        if self.is_whole_vaulted():
+            self._run_process(command='decrypt')
+        else:
+            raise Exception(f"{self.filename} is not whole vaulted file. decrypt subcommand does not support.")
+
+    def view(self):
+        if self.is_whole_vaulted():
+            self._run_process(command='view')
+        else:
+            raise Exception(f"{self.filename} is not whole vaulted file. view subcommand does not support.")
+
+def read_passfile(passfile):
+    password_sets = []
+    with open(passfile) as f:
+        for line in list(f.readlines()):
+            if line.strip() == '':
+                continue
+            if line[0] == '#':
+                continue
+            name, password = line.strip().split(',', 2)
+            password_sets.append(dict(
+                name=name, password=password
+            ))
+    return password_sets
+
+
+# https://gihyo.jp/dev/serial/01/yaml_library/0003
+# https://stackoverflow.com/questions/27518976/how-can-i-get-pyyaml-safe-load-to-handle-python-unicode-tag
+def yaml_register_class(klass, ytag):
+    suffix = '%s.%s' % (klass.__module__, klass.__name__)
+    def representer(dumper, instance):
+        node = dumper.represent_mapping(ytag, instance.__dict__)
+        return node
+    def constructor(loader, node):
+        return node.value
+    yaml.SafeDumper.add_representer(klass, representer)
+    yaml.SafeLoader.add_constructor(ytag, constructor)
+    #suffix = '%s.%s' % (klass.__module__, klass.__name__)
+    #f1 = lambda dumper, obj: dumper.represent_mapping(ytag, obj.__dict__)
+    #f2 = lambda loader, node: loader.construct_python_object(suffix, node)
+    #yaml.add_representer(klass, f1)
+    #yaml.add_constructor(ytag, f2)
+
+class VaultString():
+    def __init__(self, str):
+        self.str = 'unko'
+
+    def __str__(self) -> str:
+        return self.str
+
+def command_decrypt_yaml(args):
+    yaml_register_class(VaultString, '!vault')
+    with open(args.filename, 'r') as f:
+        obj = yaml.safe_load(f)
+        pp(obj)
+
+def command_decrypt(args):
+    password_sets = read_passfile(args.passfile)
+    av = AnsibleVault(args.filename, password_sets)
+    av.decrypt()
+
+def command_view(args):
+    password_sets = read_passfile(args.passfile)
+    av = AnsibleVault(args.filename, password_sets)
+    av.view()
+
+def main(args=None):
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers()
+    
+    parser_decrypt = subparsers.add_parser('decrypt_yaml')
+    parser_decrypt.add_argument('--passfile')
+    parser_decrypt.add_argument('filename')
+    parser_decrypt.set_defaults(handler=command_decrypt_yaml)
+
+    parser_decrypt = subparsers.add_parser('decrypt')
+    parser_decrypt.add_argument('--passfile')
+    parser_decrypt.add_argument('filename')
+    parser_decrypt.set_defaults(handler=command_decrypt)
+
+    parser_view = subparsers.add_parser('view')
+    parser_view.add_argument('--passfile')
+    parser_view.add_argument('filename')
+    parser_view.set_defaults(handler=command_view)
+
+    args = parser.parse_args(args)
+    if hasattr(args, 'handler'):
+        args.handler(args)
+    else:
+        parser.print_help()
+
+if __name__ == "__main__":
+    main()
---
+++ test_avault.py
@@ -0,0 +1,131 @@
+import pytest
+from assertpy import assert_that, fail
+import yaml
+import logging
+from avault import main
+import io
+import textwrap
+import tempfile
+import subprocess
+import os
+import sys
+
+logger = logging.getLogger(__name__)
+
+wholevault = textwrap.dedent("""
+$ANSIBLE_VAULT;1.1;AES256
+38383732363137313834383932643661656164646538646365653832613266313363653863653964
+6262663164386136376133313162326434393865333261390a333336356562343935386539616533
+61616537613536323366653063396530396136646537656465323863383365316338306430356365
+6465613932396433300a376566663361313331366433393933313931386261383133393763636662
+61333264366135613632616261313438366265656162643965663266646439373433303035346339
+63636333356365363761643038626432373330393934663633326438623036326163636161366536
+373662656362313831346230386331636464
+""")[1:-1]
+
+wholevault_decrypted = textwrap.dedent("""
+- item1
+- item2
+- key1: value1
+  key2:
+    key2-2: value2-2
+""")[1:-1]
+
+inlinevault = textwrap.dedent("""
+- item1
+- !vault |
+    $ANSIBLE_VAULT;1.1;AES256
+    30323633363634656636323338386264376561376632323135343964376332653431363132616365
+    3762633064663266623361653264383761656462323334350a616361663938343865633033336334
+    35343365656262666330613933633265326266633434313564303964663164366432666430363863
+    6534313837316538310a333334646333613164306234326563633132343536366162306533386236
+    3633
+- key1: value1
+  key2:
+    key2-2: !vault |
+            $ANSIBLE_VAULT;1.1;AES256
+            39616165333162396239363165326434613731386531666336353435633131633139633634346130
+            6230363165663034393561313937616233376439356233610a646539663731633939626366383237
+            63656130313532633531383561313966666437383634646662363763303863303034643235613833
+            6461626631613031330a626666343661666238353233353632363230393531316366303731666634
+            3862
+""")[1:-1]
+
+inlinevault_decrypted = textwrap.dedent("""
+- item1
+- item2
+- key1: value1
+  key2:
+    key2-2: value2-2
+""")[1:-1]
+
+passwords = textwrap.dedent("""
+#name1,password1
+name2,test
+""")
+
+@pytest.mark.skip(reason='pytestskip')
+class TestWholeVault():
+    @pytest.fixture(scope='function', autouse=True)
+    def scope_function(self):
+        filename = '/tmp/abcde'
+        with open(filename, 'w') as f:
+            print(wholevault, end='', file=f)
+
+        passfile = '/tmp/passfile'
+        with open(passfile, 'w') as f:
+            print(passwords, end='', file=f)
+
+        yield dict(filename=filename, passfile=passfile)
+
+    def test_decrypt(self, scope_function):
+        filename = scope_function['filename']
+        passfile = scope_function['passfile']
+
+        args = ['decrypt', '--passfile', passfile, filename]
+        main(args=args)
+        with open(filename, 'r') as f:
+            assert_that(f.read().rstrip()).is_equal_to(wholevault_decrypted.rstrip())
+
+    def test_view(self, scope_function, capfd):
+        filename = scope_function['filename']
+        passfile = scope_function['passfile']
+
+        args = ['view', '--passfile', passfile, filename]
+        main(args=args)
+        out, err = capfd.readouterr()
+        assert_that(out.rstrip()).is_equal_to(wholevault_decrypted.rstrip())
+
+
+class TestInlineVault():
+    @pytest.fixture(scope='function', autouse=True)
+    def scope_function(self):
+        filename = '/tmp/abcde'
+        with open(filename, 'w') as f:
+            print(inlinevault, end='', file=f)
+
+        passfile = '/tmp/passfile'
+        with open(passfile, 'w') as f:
+            print(passwords, end='', file=f)
+
+        yield dict(filename=filename, passfile=passfile)
+
+    def test_decrypt(self, scope_function):
+        filename = scope_function['filename']
+        passfile = scope_function['passfile']
+
+        args = ['decrypt_yaml', '--passfile', passfile, filename]
+        main(args=args)
+        with open(filename, 'r') as f:
+            assert_that(f.read().rstrip()).is_equal_to(inlinevault_decrypted.rstrip())
+
+    @pytest.mark.skip(reason='pytestskip')
+    def test_view(self, scope_function, capfd):
+        filename = scope_function['filename']
+        passfile = scope_function['passfile']
+
+        args = ['view', '--passfile', passfile, filename]
+        main(args=args)
+        out, err = capfd.readouterr()
+        assert_that(out.rstrip()).is_equal_to(inlinevault_decrypted.rstrip())
+
